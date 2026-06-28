273e248fc7016d7748df8df5aa16535e8624a382
omerfuyar <denemeyo@gmail.com>
2026-06-28 17:01:59 +0300

    -initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SHUJSN
+
+Shujsn (SHU JSON) is a single header cross platform JSON file parser.
---
+++ shujsn.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#pragma region Shucio Macros
+
+#pragma endregion Shucio Macros
+
+#pragma region Shucio Declarations
+
+#pragma endregion Shucio Declarations
+
+#pragma region Shucio Definitions
+
+#ifdef SHUJSN_IMPLEMENTATION
+#endif // SHUJSN_IMPLEMENTATION
+
+#pragma endregion Shucio Definitions
